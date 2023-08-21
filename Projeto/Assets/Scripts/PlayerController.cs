0a893e471bdfe8a90dfec6f3410fe4d785deefed
Carlos V. B. Dos Santos <121064459+Carlos-v-b-Santos@users.noreply.github.com>
2023-08-21 17:41:24 -0400

    17:42h_21/08

--- Projeto/Assets/Scripts/PlayerController.cs
+++ Projeto/Assets/Scripts/PlayerController.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public float speed = 20;
+    [SerializeField] float speed = 10;
+    Rigidbody2D rigidbody2d;
+    float horizontalMove;
+    float verticalMove;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerMove();
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
     }
 
-    void playerMove()
+    void FixedUpdate()
     {
-        float horizontalMove;
-        float verticalMove;
-        horizontalMove = Time.deltaTime * speed * Input.GetAxis("Horizontal");
-        verticalMove = Time.deltaTime * speed * Input.GetAxis("Vertical");
-        this.transform.Translate(horizontalMove,verticalMove,0);
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontalMove * Time.deltaTime;
+        position.y = position.y + speed * verticalMove * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
     }
+    //void playerMove()
+
 }
+    
