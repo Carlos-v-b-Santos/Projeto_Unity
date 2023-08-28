91123f0dd20a5b9811044939095b15705c4e90ab
Carlos V. B. Dos Santos <121064459+Carlos-v-b-Santos@users.noreply.github.com>
2023-08-28 16:55:27 -0400

    inputSystem

--- Projeto/Assets/Scripts/PlayerController.cs
+++ Projeto/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 10;
     Rigidbody2D rigidbody2d;
-    float horizontalMove;
-    float verticalMove;
+    //float horizontalMove;
+    //float verticalMove;
+    Vector2 playerMove;
+
+    public void SetMove(InputAction.CallbackContext value)
+    {
+        playerMove = value.ReadValue<Vector2>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -17,15 +24,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxis("Horizontal");
-        verticalMove = Input.GetAxis("Vertical");
+        //horizontalMove = Input.GetAxis("Horizontal");
+        //verticalMove = Input.GetAxis("Vertical");
     }
 
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * horizontalMove * Time.deltaTime;
-        position.y = position.y + speed * verticalMove * Time.deltaTime;
+        position += speed * Time.deltaTime * playerMove;
+        //position.x = position.x + speed * horizontalMove * Time.deltaTime;
+        //position.y = position.y + speed * verticalMove * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
