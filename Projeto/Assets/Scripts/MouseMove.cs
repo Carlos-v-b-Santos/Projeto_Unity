e22cba46a3be8c248ea575c265784b79286a77a6
Carlos V. B. Dos Santos <121064459+Carlos-v-b-Santos@users.noreply.github.com>
2023-09-07 10:21:09 -0400

    10:20_-7/09/23
    movimentação por mouse com navMeshPlus

--- Projeto/Assets/Scripts/MouseMove.cs
+++ Projeto/Assets/Scripts/MouseMove.cs
@@ -6,67 +6,49 @@ using UnityEngine.AI;
 
 public class MouseMove : MonoBehaviour
 {
-    private Camera _camera;
+    private Camera mainCamera;
     private NavMeshAgent agent;
 
-    private Vector3 _moveTarget = Vector3.zero;
-    private Vector3 _direction = Vector3.zero;
+    private Vector2 movePlayer;
 
     [SerializeField] float speed = 2f;
     // Start is called before the first frame updat
 
-    private PlayerInputActions _inputMapping;
-    private void Awake() => _inputMapping = new PlayerInputActions();
+    private PlayerInputActions input;
+    private void Awake() => input = new PlayerInputActions();
     void Start()
     {
+        
+
         agent = GetComponent<NavMeshAgent>();
         agent.speed = 4f;
         agent.updateRotation = false;
         agent.updateUpAxis = false;
 
-        _camera = Camera.main; 
+        mainCamera = Camera.main; 
 
-        _inputMapping.Player.Click.performed += Click;
+        input.Player.Click.performed += Click;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Dot(_direction, transform.forward) >= .99f) 
-        {
-           agent.SetDestination(_moveTarget);
-        }
+        agent.SetDestination(movePlayer);
     }
 
     private void Click(InputAction.CallbackContext context) 
     {
-        Debug.Log("Click");
-
-        Ray ray = _camera.ScreenPointToRay(Mouse.current.position.ReadValue());
-
-        if (Physics.Raycast(ray, out RaycastHit hit, 50f)) {
-            //Physics.Raycast(ray, out RaycastHit hit, 500000f);
-
-            Debug.Log("Click2");
-            if (NavMesh.SamplePosition(hit.point, out NavMeshHit navPos, .25f, 1 << 0)) {
-                Debug.Log("Click3");
-                _moveTarget = navPos.position;
-                Debug.Log(_moveTarget);
-                //_direction = (_moveTarget - transform.position).normalized;
-            
-                //StopNavigation();
-            
-                //if (Vector3.Dot(_direction, transform.forward) >= 0.25f) {
-                    agent.SetDestination(_moveTarget);
-                //}
-            }
+        Vector2 point = mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        RaycastHit2D hit = (Physics2D.Raycast(point, Vector2.zero));
+        
+        if(hit.collider != null) {
+            movePlayer = hit.point;
         }
     }
 
     private void StopNavigation() {
         agent.SetDestination(transform.position);
     }
-    private void Run(InputAction.CallbackContext context) { }
-    private void OnEnable() => _inputMapping.Enable();
-    private void OnDisable() => _inputMapping.Disable();
+    private void OnEnable() => input.Enable();
+    private void OnDisable() => input.Disable();
 }
